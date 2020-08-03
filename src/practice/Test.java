0537f6b6b7994ebb2b87920552465d75c17f0162
Ganga <erishab1@gmail.com>
2020-08-02 21:32:29 -0500

    G-commit

--- src/practice/Test.java
+++ src/practice/Test.java
@@ -5,33 +5,32 @@ public class Test {
 	/***
 	 * Write a Java program to find the longest substring from a given string which doesn�t contain any duplicate characters                            
 	 */
-	public static boolean uniqueWord(String s) {
-		for(int i=0; i<s.length();i++) 
-			for(int j=i+1; j<s.length();j++) 
-				if(s.charAt(i)==s.charAt(j)) {
-					return false;
-				}
-		return true;		
-	}
 	public static void main(String[] args) {
-			String str =  "Hello how are fine you doin";
-			String strArray [] = str.toLowerCase().split(" ");
-			String uniqueStr="";
-			int length=0;
-			for(String s: strArray) {
-				System.out.println(s);
-				if(uniqueWord(s)) {
-					if(length<s.length()) {
-					length=s.length();
-					uniqueStr=s+" ";
-					}else if(length==s.length()) {
-						uniqueStr+=s+" ";
-					}
-				}else {
-					length=0;
-					uniqueStr="";
-				}
-			}
-			System.out.println("'"+str + "' Unique Longest Substring : "+ uniqueStr);
-		} 
+		String str = "selenium and coding";
+		str.replaceAll(" ", "");
+		char[] words = str.toCharArray();
+
+		String longestChar = null;
+		int sizeOfSubstring = 0;
+
+		Map<Character, Integer> map = new LinkedHashMap<>();
+
+		for (Character word : words) {
+			Integer count = map.get(word);
+			if (count == null) {
+				map.put(word, 1);
+			} else
+				map.put(word, ++count);
+		}
+
+		if (map.size() > sizeOfSubstring) {
+			sizeOfSubstring = map.size();
+			longestChar = map.keySet().toString();
+		}
+
+		System.out.println(str);
+		System.out.println(sizeOfSubstring);
+		System.out.println(longestChar);
+	}
+
 }
